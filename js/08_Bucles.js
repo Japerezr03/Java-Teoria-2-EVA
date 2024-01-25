f3b45759bde4de54c1bee080a5782c6e079358fc
japerezr03 <japerezr03.com@hotmail.com>
2024-01-25 09:52:30 +0100

    j

---
+++ js/08_Bucles.js
@@ -0,0 +1,13 @@
+/*
+const tabla = Number(prompt("Dame un número"));
+console.log("Tabla del:" + tabla);
+for (let i = 0; i < 11; i++) {
+    console.log(tabla + " * " + i + "= " + (tabla * i));
+}
+*/
+
+//Cuenta atras
+for (let i = 10; i > -1; i--) {
+    console.log(i);
+}
+console.log("Lanzando nave");
