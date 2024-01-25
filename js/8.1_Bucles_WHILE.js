a5b143de8ffdf42885dd758d199ac04a3b1a609d
japerezr03 <japerezr03.com@hotmail.com>
2024-01-25 10:18:51 +0100

    s

---
+++ js/8.1_Bucles_WHILE.js
@@ -0,0 +1,7 @@
+//WHILE Mientras que se cumpla la condicion ejecuta {}
+let contador = 0
+while (contador <= 10) {
+    console.log("Valor del contador", contador);
+    //Tenemos que hacer algo para que llegue a cumplirse la condicion
+    contador++; //Contador = contador + 1
+}
