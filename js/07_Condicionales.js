413906b33eb0e58f90c3c0c1655ea9d6ba3c1241
japerezr03 <japerezr03.com@hotmail.com>
2024-01-25 08:53:28 +0100

    a

--- js/07_Condicionales.js
+++ js/07_Condicionales.js
@@ -1,6 +1,5 @@
 //IF
-
-const edad = Number(prompt("Dame tu edad"))
+const edad = Number(prompt("Dame tu edad"));
 
 //edad > 17 es lo mismo que decir edad >=18
 if (edad >= 18) {
@@ -9,12 +8,27 @@ if (edad >= 18) {
 console.log("Tu edad es:", edad); //Tu edad es: " + edad
 
 //IF-ELSE
-let edad1 = Number(prompt("Dame tu edad"))
+let edad1 = Number(prompt("Dame tu edad"));
 
 //edad > 17 es lo mismo que decir edad >=18
 if (edad1 >= 18) {
     console.log("Puedes votar");
 } else {
-    console.log("Debes eperar para votar")
+    console.log("Debes eperar para votar");
 }
 console.log("Tu edad es:", edad1); //Tu edad es: " + edad
+
+//SWICH
+const nota = Number(prompt("Nota del examen"));
+switch (nota) {
+    case 4: case 3: case 2: case 1: case 0:
+        console.log("Tienes que hacer la recuperación");
+        //Hay que porner el break para que no siga ejecutando
+        break;
+    //Por defecto
+    default:
+        console.log("Te vas a hacer la FCT");
+        break;
+}
+
+//Mensaje para cada día de la semana
